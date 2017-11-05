2f79ff918ab52cd5d6dc7ed11a466a8ea890786f
Samal Roman <roman.samal.bs@gmail.com>
2017-11-05 20:44:58 +0300

    Added 2 new modes.

--- LED_CUBE_8x8x8.ino
+++ LED_CUBE_8x8x8.ino
@@ -94,7 +94,7 @@ void mode1()
     setMask(MASK);
     writeRegister();
     latchRegister();
-    delay(200);
+    delay(40);
   }
   for (int i = 0; i < 8; i++)
   {
@@ -111,14 +111,102 @@ void mode1()
     setMask(MASK);
     writeRegister();
     latchRegister();
-    delay(100);
+    delay(40);
   }
 }
 
+void mode2()
+{
+   enableOutput();
+   for (int i = 0; i < 8; i++)
+   {
+    if (i % 2 == 0)
+    {
+      MASK.level = HIGH;
+      MASK.col0 = LOW;
+      MASK.col1 = HIGH;
+      MASK.col2 = LOW;
+      MASK.col3 = HIGH;
+      MASK.col4 = LOW;
+      MASK.col5 = HIGH;
+      MASK.col6 = LOW;
+      MASK.col7 = HIGH;
+      setMask(MASK);
+    }
+    else
+    {
+      MASK.level = LOW;
+      MASK.col0 = HIGH;
+      MASK.col1 = LOW;
+      MASK.col2 = HIGH;
+      MASK.col3 = LOW;
+      MASK.col4 = HIGH;
+      MASK.col5 = LOW;
+      MASK.col6 = HIGH;
+      MASK.col7 = LOW;
+      setMask(MASK);
+    }
+    writeRegister();
+    latchRegister();
+    delay(100);
+    MASK.level = HIGH;
+    MASK.col0 = HIGH;
+    MASK.col1 = HIGH;
+    MASK.col2 = HIGH;
+    MASK.col3 = HIGH;
+    MASK.col4 = HIGH;
+    MASK.col5 = HIGH;
+    MASK.col6 = HIGH;
+    MASK.col7 = HIGH;
+    setMask(MASK);
+    writeRegister();
+    latchRegister();
+    delay(100);
+   }   
+}
+
+void mode3()
+{
+   enableOutput();
+   byte randomNum = random(2);
+   for (int i = 0; i < 20; i++)
+   {
+      MASK.level = randomNum;
+      randomNum = random(2);
+      MASK.col0 = randomNum;
+      randomNum = random(2);
+      MASK.col1 = randomNum;
+      randomNum = random(2);
+      MASK.col2 = randomNum;
+      randomNum = random(2);
+      MASK.col3 = randomNum;
+      randomNum = random(2);
+      MASK.col4 = randomNum;
+      randomNum = random(2);
+      MASK.col5 = randomNum;
+      randomNum = random(2);
+      MASK.col6 = randomNum;
+      randomNum = random(2);
+      MASK.col7 = randomNum;
+      setMask(MASK);
+      writeRegister();
+   }
+   latchRegister();
+   delay(70);
+}
+
 void loop() {
-  mode1();
-  mode1();
-  mode1();
-  mode1();
-  delay(500);
+  for (int i = 0; i < 10; i++)
+  {
+    mode1();
+  }
+  
+  for (int i = 0; i < 8; i++)
+  {
+    mode2();
+  }
+  for (int i = 0; i < 80; i++)
+  {
+    mode3();
+  }
 }
